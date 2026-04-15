4620d3297a22b71e4931af70c70404efbde60015
consu <consuelo.rodriguez@pregrado.uoh.cl>
2026-04-15 15:13:58 -0400

    reparando

--- 2da fase/get_cpoints.py
+++ 2da fase/get_cpoints.py
@@ -2,6 +2,34 @@ import numpy as np
 from vol_star import  _choose_batch
 from scipy.spatial import ConvexHull
 
+#primero, averiguar cómo pasar de H representation a V representation
+
+import cdd
+
+def obtener_vertices_desde_paredes(A, b):
+    # Combinamos b y A en el formato que pide CDD: [b | -A]
+    mat_cdd = np.hstack([np.array(b).reshape(-1, 1), -np.array(A)])
+    
+    # Creamos la matriz de CDD
+    mat = cdd.Matrix(mat_cdd, number_type='float')
+    mat.rep_type = cdd.RepType.INEQUALITY
+    
+    # El algoritmo de Doble Descripción calcula los vértices
+    poly = cdd.Polyhedron(mat)
+    gen = poly.get_generators()
+    
+    # Retornamos solo los puntos (ignorando rayos si los hubiera)
+    return np.array(gen)[:, 1:]
+
+
+
+
+
+
+
+
+
+
 #usando primero la de random points, o asegurando
 #que los puntos tengan como primera coordenada las z
 def get_centerpoints(verts):
@@ -13,18 +41,21 @@ def get_centerpoints(verts):
     """
     verts = np.asarray(verts)
 
-    #primero obtener la envolvente convexa global
+    """ #primero obtener la envolvente convexa global
     hull_3d = ConvexHull(verts, qhull_options= 'QJ')
     A = hull_3d.equations[:, :-1]
     b = hull_3d.equations[:, -1]
 
-    centroides = []
+    todo esto no hace nada"""
 
+    centroides = []
     #ahora procesamos cada fibra
     for z_val in [0,1,2]:
-        #reducir las inecuaciones de 3d a 2d para esta z en específico
+        """#reducir las inecuaciones de 3d a 2d para esta z en específico
         Ap = A[:,1:]
         b_shift = b - A[:,0]*z_val
+
+        esto tampoco hace nada"""
 
         #obtenemos los vértices del polígono 2d en esta rodaja
         mask = (verts[:,0] == z_val)
@@ -33,15 +64,15 @@ def get_centerpoints(verts):
         hull_2d = ConvexHull(puntos_slice)
         puntos = puntos_slice[hull_2d.vertices]
         
-        n = len(p)
+        n = puntos.shape[0]
 
         area = 0
         cx = 0
         cy = 0
         
         for i in range(n):
-            x0, y0 = p[i]
-            x1, y1 = p[(i + 1) % n]
+            x0, y0 = puntos[i]
+            x1, y1 = puntos[(i + 1) % n]
             
             f_comun = (x0 * y1 - x1 * y0)
             area += f_comun
@@ -50,7 +81,7 @@ def get_centerpoints(verts):
             
         area *= 0.5
         # Evitar división por cero
-        if abs(area) < 1e-10:
+        if abs(area) < 1e-12:
             c_final = np.mean(p, axis=0)
             centroides.append([z_val, float(c_final[0]), float(c_final[1])])
         else:
@@ -62,70 +93,3 @@ def get_centerpoints(verts):
     centroides.append(va)
     
     return centroides[0], centroides[1], centroides[2], centroides[3]
-    
-
-
-
-
-
-def get_cpoints(
-        A, #matriz
-        b, #vector
-        d= 2, #dimensiones continuas
-        z_vals = [0,1,2], #valores para z dimensión entera
-        N=10**7, #número de puntos aleatorios a generar para cada slice
-        batch = None, #tamaño de lote para la generación de puntos aleatorios
-        target_mb = None, #memoria target para calcular el tamaño de lote
-        tol = 1e-9 #tolerancia
-    ):
-    
-    """
-    Devuelve
-    -----------
-    v0, v1, v2 y va, puntos que nos interesan
-    """
-
-    n_ineq = A.shape[0]
-
-    #para controlar la memoria RAM, lo mismo que hizo la María
-    if batch is None:
-        m_auto = _choose_batch(n_ineq, target_mb=target_mb)
-        batch = min(N, max(1000, m_auto))
-    else:
-        batch = int(batch)
-        if batch <= 0:
-            batch = min(N, max(1000, _choose_batch(n_ineq, target_mb=target_mb)))
-
-
-
-    centroides = []
-    for z in z_vals:
-        Az = A[:,0] #sólo primera columna de A
-        Ap = A[:,1:] #le saco la primera columna a A
-        b_p = b - Az*z #ahora tenemos la desigualdad para un figura 2D
-        #Ax*x + Ay*y <= b - Az*z
-        #siendo z la primera coordenada fija
-
-
-        gen = 0
-        points_inside = []
-        #para cada z, encuentra el centroide de la slice definida por Ap y b_p
-        while gen < N:
-            m = min(batch, N-gen)
-            p = np.random.rand(m,d) #generamos puntos aleatorios entre 0 y 1
-            inside = np.all((p @ Ap.T) <= (b_p + tol), axis=1) 
-            #verificamos cuáles caen dentro de la slice
-            if inside.any():
-                points_inside.append(p[inside]) #guardamos los que caen dentro
-            gen += m
-        
-        if points_inside:
-            points_inside = np.vstack(points_inside) #unimos todos los puntos dentro
-            centroide = points_inside.mean(axis=0) #calculamos el centroide de esos puntos
-            centroides.append(np.concatenate([[z], centroide])) #guardamos el centroide con su z correspondiente
-
-        #ahora falta agregar el 4to punto de interés
-        #el promedio entre el centroide de la slice 0 y de la slice 2
-    
-    v_a = (centroides[0] + centroides[2]) / 2
-    return centroides[0], centroides[1], centroides[2], v_a
--- 2da fase/previous_cpoints.py
+++ 2da fase/previous_cpoints.py
@@ -48,3 +48,67 @@ def get_centerpoints(verts):
     v = [va, vb, vc]
     centroides.append(v)
     return centroides
+
+
+
+def get_cpoints(
+        A, #matriz
+        b, #vector
+        d= 2, #dimensiones continuas
+        z_vals = [0,1,2], #valores para z dimensión entera
+        N=10**7, #número de puntos aleatorios a generar para cada slice
+        batch = None, #tamaño de lote para la generación de puntos aleatorios
+        target_mb = None, #memoria target para calcular el tamaño de lote
+        tol = 1e-9 #tolerancia
+    ):
+    
+    """
+    Devuelve
+    -----------
+    v0, v1, v2 y va, puntos que nos interesan
+    """
+
+    n_ineq = A.shape[0]
+
+    #para controlar la memoria RAM, lo mismo que hizo la María
+    if batch is None:
+        m_auto = _choose_batch(n_ineq, target_mb=target_mb)
+        batch = min(N, max(1000, m_auto))
+    else:
+        batch = int(batch)
+        if batch <= 0:
+            batch = min(N, max(1000, _choose_batch(n_ineq, target_mb=target_mb)))
+
+
+
+    centroides = []
+    for z in z_vals:
+        Az = A[:,0] #sólo primera columna de A
+        Ap = A[:,1:] #le saco la primera columna a A
+        b_p = b - Az*z #ahora tenemos la desigualdad para un figura 2D
+        #Ax*x + Ay*y <= b - Az*z
+        #siendo z la primera coordenada fija
+
+
+        gen = 0
+        points_inside = []
+        #para cada z, encuentra el centroide de la slice definida por Ap y b_p
+        while gen < N:
+            m = min(batch, N-gen)
+            p = np.random.rand(m,d) #generamos puntos aleatorios entre 0 y 1
+            inside = np.all((p @ Ap.T) <= (b_p + tol), axis=1) 
+            #verificamos cuáles caen dentro de la slice
+            if inside.any():
+                points_inside.append(p[inside]) #guardamos los que caen dentro
+            gen += m
+        
+        if points_inside:
+            points_inside = np.vstack(points_inside) #unimos todos los puntos dentro
+            centroide = points_inside.mean(axis=0) #calculamos el centroide de esos puntos
+            centroides.append(np.concatenate([[z], centroide])) #guardamos el centroide con su z correspondiente
+
+        #ahora falta agregar el 4to punto de interés
+        #el promedio entre el centroide de la slice 0 y de la slice 2
+    
+    v_a = (centroides[0] + centroides[2]) / 2
+    return centroides[0], centroides[1], centroides[2], v_a
