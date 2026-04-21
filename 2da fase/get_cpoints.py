f63b45118a43325314545beb34bbb3daccebea35
consu <consuelo.rodriguez@pregrado.uoh.cl>
2026-04-20 22:49:39 -0400

    h_to_v_rep lista

--- 2da fase/get_cpoints.py
+++ 2da fase/get_cpoints.py
@@ -1,11 +1,8 @@
 import numpy as np
-from vol_star import  _choose_batch
-from scipy.spatial import ConvexHull
-
-#primero, averiguar cómo pasar de H representation a V representation
+from itertools import combinations
 
 
-def h_to_v_rep(A, b, tol = 1e-9):
+def h_to_v_rep(A, b):
     """
     Obtiene la V-representation de un cuerpo convexo a partir de la H-representation
     A : Matriz de coeficientes (m x n)
@@ -17,10 +14,22 @@ def h_to_v_rep(A, b, tol = 1e-9):
     # Buscamos intersecciones de 3 planos
     for indice in combinations(range(m),n):
         # Seleccionamos las 3 filas correspondientes a los planos
+        A_sub = A[list(indice)]
+        b_sub = b[list(indice)]
 
+        punto = np.linalg.solve(A_sub, b_sub)
 
-    return vertices
-
+        # Verificar validez del punto encontrado
+        if np.all(np.dot(A, punto) <= b):
+            # Verificar unicidad del punto, si es único => append
+            new_point = True
+            for v in vertices:
+                if np.allclose(v, punto):
+                    new_point = False
+            if new_point:
+                vertices.append(punto)
+        
+    return np.asarray(vertices)
 
 
 
